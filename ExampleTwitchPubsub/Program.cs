e99e1a7eb28c3c41e8779259e9bb34fa7f043718
JayJay1989BE <jonas@lateur.pro>
2021-01-21 21:26:46 +0100

    Initial Project commit
    This will work with latest prerelease nuget package (01/21/2021)

---
+++ ExampleTwitchPubsub/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+
+namespace ExampleTwitchPubsub
+{
+    class Program
+    {
+        private static ILogger _logger;
+        public static IConfiguration Settings;
+
+        static void Main(string[] args)
+        {            
+            var outputTemplate = "[{Timestamp:HH:mm:ss} {Level}] {Message}{NewLine}{Exception}";
+            _logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: outputTemplate)
+                .WriteTo.File("log/log_.txt", outputTemplate: outputTemplate, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            Settings = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("Settings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+            new Program().MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        async Task MainAsync(string[] args)
+        {
+
+        }
+    }
+}
